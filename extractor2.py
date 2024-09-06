2051ad04727d5f3446449fafb60bfcbf43fb477a
AxlJoan <60158935+AxlJoan@users.noreply.github.com>
2024-09-06 11:01:43 -0500

    Mejoras extractor2.py

--- extractor2.py
+++ extractor2.py
@@ -27,32 +27,37 @@ def get_or_prompt_config():
 # Uso de la función para obtener la configuración
 config = get_or_prompt_config()
 
-# Conexión a la base de datos msgstore.db y lectura de datos
-con = sqlite3.connect('/sdcard/msgstore.db')
+# Conexión y lectura de la base de datos msgstore.db
 try:
-    chv = pd.read_sql_query("SELECT * from chat_view", con)
-except pd.io.sql.DatabaseError:
-    chv = None  # En caso de que el query no devuelva resultados
+    with sqlite3.connect('/sdcard/msgstore.db') as con:
+        try:
+            chv = pd.read_sql_query("SELECT * from chat_view", con)
+        except pd.io.sql.DatabaseError:
+            chv = None  # En caso de que el query no devuelva resultados
+        
+        usuarios = pd.read_sql_query("SELECT * from 'jid'", con)
+        msg = pd.read_sql_query("SELECT * from message", con)
+except sqlite3.Error as e:
+    print(f"Error conectando a la base de datos msgstore.db: {e}")
+    exit(1)
 
-usuarios = pd.read_sql_query("SELECT * from 'jid'",con)
+# Conexión y lectura de la base de datos wa.db
+try:
+    with sqlite3.connect('/sdcard/wa.db') as con1:
+        contacts = pd.read_sql_query("SELECT * from wa_contacts", con1)
+        contacts['jid'] = contacts['jid'].str.split('@').str[0]
 
-msg = pd.read_sql_query("SELECT * from message", con)
-con.close()
+        descriptions = pd.read_sql_query("SELECT * FROM wa_group_descriptions", con1)
+        descriptions['jid'] = descriptions['jid'].str.split('@').str[0]
 
-# Conexión a la base de datos wa.db y lectura de datos
-con1 = sqlite3.connect('/sdcard/wa.db')
-contacts = pd.read_sql_query("SELECT * from wa_contacts", con1)
-contacts['jid'] = contacts['jid'].str.split('@').str[0]
+        names = pd.read_sql_query("SELECT * from wa_vnames", con1)
+        names['jid'] = names['jid'].str.split('@').str[0]
+except sqlite3.Error as e:
+    print(f"Error conectando a la base de datos wa.db: {e}")
+    exit(1)
 
-descriptions = pd.read_sql_query("SELECT * FROM wa_group_descriptions", con1)
-descriptions['jid'] = descriptions['jid'].str.split('@').str[0]
-
-names = pd.read_sql_query("SELECT * from wa_vnames", con1)
-names['jid'] = names['jid'].str.split('@').str[0]
-con1.close()
-
-usuarios['user'] = usuarios['user'].astype(str)
-usuarios['user'] = usuarios['user'].str[3:]
+# Procesamiento de datos
+usuarios['user'] = usuarios['user'].astype(str).str[3:]
 usuarios['server'] = usuarios['server'].apply(lambda x: 'celular' if x.endswith('.net') else ('grupo' if x.endswith('.us') else 'otro'))
 
 # Pre-procesamiento de msg
@@ -61,150 +66,126 @@ msg = msg.dropna(subset=['text_data'])  # Eliminar filas donde text_data es NaN
 msg['timestamp'] = pd.to_datetime(msg['timestamp'], unit='ms')
 msg['received_timestamp'] = pd.to_datetime(msg['received_timestamp'], unit='ms')
 
-# Función para mapear chat_row_id a número de teléfono
 def mapping(id):
     phone = chv.loc[chv['_id'] == id, 'raw_string_jid'].iloc[0]
-    phone = phone.split('@')[0]
-    return phone
+    return phone.split('@')[0]
 
 def mapping2(id):
-    phone2 = usuarios.loc[usuarios['_id'] == id, 'user'].iloc[0]
-    return phone2
+    return usuarios.loc[usuarios['_id'] == id, 'user'].iloc[0]
 
 def mapping3(id):
-    server = usuarios.loc[usuarios['_id'] == id, 'server'].iloc[0]
-    return server
+    return usuarios.loc[usuarios['_id'] == id, 'server'].iloc[0]
 
 def mapping4(id):
-    server = usuarios.loc[usuarios['_id'] == id, 'device'].iloc[0]
-    return server
+    return usuarios.loc[usuarios['_id'] == id, 'device'].iloc[0]
 
 def mapping5(id):
-    name = chv.loc[chv['_id'] == id, 'subject'].iloc[0]
-    return name
+    return chv.loc[chv['_id'] == id, 'subject'].iloc[0]
 
-
-
-msg['number'] = msg['chat_row_id'].apply(lambda x: mapping(x))
-msg['number2'] = msg['chat_row_id'].apply(lambda x: mapping2(x))
+msg['number'] = msg['chat_row_id'].apply(mapping)
+msg['number2'] = msg['chat_row_id'].apply(mapping2)
 msg = pd.merge(msg, contacts[['jid', 'status']], left_on='number2', right_on='jid', how='left').drop('jid', axis=1)
 msg = pd.merge(msg, names[['jid', 'verified_name']], left_on='number2', right_on='jid', how='left').drop('jid', axis=1)
-msg['server'] = msg['chat_row_id'].apply(lambda x: mapping3(x))
-msg['device'] = msg['chat_row_id'].apply(lambda x: mapping4(x))
-msg['group'] = msg['chat_row_id'].apply(lambda x: mapping5(x))
+msg['server'] = msg['chat_row_id'].apply(mapping3)
+msg['device'] = msg['chat_row_id'].apply(mapping4)
+msg['group'] = msg['chat_row_id'].apply(mapping5)
 msg = pd.merge(msg, descriptions[['jid', 'description']], left_on='number', right_on='jid', how='left').drop('jid', axis=1)
 
-# Reemplazando NaN por None para los campos enriquecidos
+# Reemplazar NaN por None para los campos enriquecidos
 msg = msg.where(pd.notnull(msg), None)
 
 def remove_emojis(text):
-    # Verifica si el texto es None antes de procesarlo
+    """Elimina emojis de un texto si no es None."""
     if text is None:
-        return text  # O puedes devolver una cadena vacía si prefieres: return ''
-    # Si no es None, procede a eliminar los emojis
+        return text  # O devolver una cadena vacía si prefieres: return ''
     return emoji.replace_emoji(text, replace='')
 
-
-# Asumiendo que msg es tu DataFrame y ya está definido
 msg['text_data'] = msg['text_data'].apply(remove_emojis)
 msg['description'] = msg['description'].apply(remove_emojis)
 msg['group'] = msg['group'].apply(remove_emojis)
-msg['timestamp'] = pd.to_datetime(msg['timestamp'], format='%m/%d/%Y %I:%M:%S %p')
+
+# Asegurarse de que las fechas están en el formato correcto
+msg['timestamp'] = pd.to_datetime(msg['timestamp']).dt.strftime('%Y-%m-%d %H:%M:%S')
+msg['received_timestamp'] = pd.to_datetime(msg['received_timestamp']).dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# Añadir configuración al DataFrame
 msg['cliente'] = config['cliente']
 msg['estado'] = config['estado']
 msg['municipio'] = config['municipio']
-msg['received_timestamp'] = pd.to_datetime(msg['received_timestamp'], format='%m/%d/%Y %I:%M:%S %p')
 
+# Guardar datos en un archivo CSV
 csv_file_path = 'messages_processed.csv'
 msg.to_csv(csv_file_path, index=False)
-# Define tus datos de conexión a MySQL
+
+# Datos de conexión a MySQL
 MYSQL_USER = "admin"
 MYSQL_PASS = "F@c3b00k"
 MYSQL_HOST = "158.69.26.160"
 MYSQL_DB = "data_wa"
 
-# Reabre la conexión a MySQL
-mysql_con = mysql.connector.connect(
-    host=MYSQL_HOST,
-    user=MYSQL_USER,
-    password=MYSQL_PASS,
-    database=MYSQL_DB
-)
-
-# Cursor para ejecutar las consultas
-cursor = mysql_con.cursor()
-
-# Crear la tabla en MySQL si no existe
-MYSQL_USER = "admin"
-MYSQL_PASS = "F@c3b00k"
-MYSQL_HOST = "158.69.26.160"
-MYSQL_DB = "data_wa"
-
-mysql_con = mysql.connector.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASS, database=MYSQL_DB)
-cursor = mysql_con.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS extraccion4 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    chat_row_id INT,
-    timestamp DATETIME,
-    received_timestamp DATETIME,
-    text_data TEXT,
-    from_me BOOLEAN,
-    number VARCHAR(255),
-    number2 VARCHAR(255),
-    status VARCHAR(255),
-    verified_name VARCHAR(255),
-    server VARCHAR(255),
-    device VARCHAR(255),
-    group_name VARCHAR(255),          
-    description TEXT,
-    cliente VARCHAR(255),
-    estado VARCHAR(255),
-    municipio VARCHAR(255)
-)
-""")
-
-# Preparar la consulta SQL para insertar los datos en MySQL
-add_message = """
-INSERT INTO extraccion4
-(chat_row_id, timestamp, received_timestamp, text_data, from_me, number, number2, status, verified_name, server, device, group_name, description, cliente, estado, municipio) 
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-"""
-
-
-msg['timestamp'] = msg['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
-msg['received_timestamp'] = msg['received_timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
-
-# Preparar los datos para la inserción
-data_to_insert = [
-    (
-        row['chat_row_id'],
-        row['timestamp'],
-        row['received_timestamp'],
-        row['text_data'],
-        row['from_me'],  # Convertido a booleano si es necesario
-        row['number'],
-        row['number2'],
-        row['status'],
-        row['verified_name'],
-        row['server'],
-        row['device'],
-        row['group'],
-        row['description'],
-        row['cliente'],
-        row['estado'],
-        row['municipio']
-    ) for index, row in msg.iterrows()
-]   
-cursor.executemany(add_message, data_to_insert)
-
-# Hacer commit de la transacción
-mysql_con.commit()
-
-# Cerrar el cursor y la conexión
-cursor.close()
-mysql_con.close()
+try:
+    mysql_con = mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASS,
+        database=MYSQL_DB
+    )
+    with mysql_con.cursor() as cursor:
+        # Crear la tabla en MySQL si no existe
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS extraccion4 (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            chat_row_id INT,
+            timestamp DATETIME,
+            received_timestamp DATETIME,
+            text_data TEXT,
+            from_me BOOLEAN,
+            number VARCHAR(255),
+            number2 VARCHAR(255),
+            status VARCHAR(255),
+            verified_name VARCHAR(255),
+            server VARCHAR(255),
+            device VARCHAR(255),
+            group_name VARCHAR(255),
+            description TEXT,
+            cliente VARCHAR(255),
+            estado VARCHAR(255),
+            municipio VARCHAR(255)
+        )
+        """)
+        
+        # Preparar la consulta SQL para insertar los datos
+        add_message = """
+        INSERT INTO extraccion4
+        (chat_row_id, timestamp, received_timestamp, text_data, from_me, number, number2, status, verified_name, server, device, group_name, description, cliente, estado, municipio) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """
+        
+        data_to_insert = [
+            (
+                row['chat_row_id'],
+                row['timestamp'],
+                row['received_timestamp'],
+                row['text_data'],
+                row['from_me'],  # Convertido a booleano si es necesario
+                row['number'],
+                row['number2'],
+                row['status'],
+                row['verified_name'],
+                row['server'],
+                row['device'],
+                row['group'],
+                row['description'],
+                row['cliente'],
+                row['estado'],
+                row['municipio']
+            ) for index, row in msg.iterrows()
+        ]
+        cursor.executemany(add_message, data_to_insert)
+        mysql_con.commit()
+except mysql.connector.Error as e:
+    print(f"Error conectando a MySQL: {e}")
+finally:
+    mysql_con.close()
 
 print("Tabla creada (si no existía) y datos subidos con éxito.")
-
