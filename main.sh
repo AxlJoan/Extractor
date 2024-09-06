068f33b49180d4bd1f220cc94a90893fec783296
AxlJoan <60158935+AxlJoan@users.noreply.github.com>
2024-09-06 10:57:33 -0500

    Optimización de main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+# Mantener el dispositivo despierto
 termux-wake-lock
 
+# Esperar un minuto
 sleep 60
 
-sh /data/data/com.termux/files/home/extractor/sacar.sh
+# Ejecutar el script combinado
+sh /data/data/com.termux/files/home/extractor/sacarCombinado.sh
 
-sh /data/data/com.termux/files/home/extractor/sacar2.sh
-
+# Ejecutar el script Python
 python /data/data/com.termux/files/home/extractor/extractor2.py
+
+# Liberar el bloqueo de la pantalla
 termux-wake-unlock
 
+# Iniciar el servicio de cron
 crond start
