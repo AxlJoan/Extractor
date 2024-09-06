a2c56444edf3f690b4820fda579934336b182e4e
AxlJoan <60158935+AxlJoan@users.noreply.github.com>
2024-09-06 11:43:01 -0500

    Update sacarCombinado.sh

--- sacarCombinado.sh
+++ sacarCombinado.sh
@@ -1,31 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Función para copiar archivos
-copiar_archivos() {
-  local ruta=$1
-  local destino=$2
+# Copiar archivos desde la ubicación correcta de la instancia LDPlayer
+echo "Copiando archivos desde com.whatsapp..."
+su -c 'cp /data/data/com.whatsapp/databases/msgstore.db /sdcard/msgstore.db'
+su -c 'cp /data/data/com.whatsapp/databases/wa.db /sdcard/wa.db'
 
-  su -c "cp $ruta/msgstore.db $destino/msgstore.db"
-  su -c "cp $ruta/wa.db $destino/wa.db"
-
-  echo "Archivos copiados desde $ruta a $destino."
-}
-
-# Primera ruta de copia
-ruta1="/data/data/com.whatsapp.w4b/databases"
-destino="/sdcard"
-
-# Segunda ruta de copia
-ruta2="/data/data/com.whatsapp/databases"
-
-# Verificar si la primera ruta existe
-if [ -d "$ruta1" ]; then
-  copiar_archivos "$ruta1" "$destino"
-else
-  # Si la primera ruta no existe, intenta con la segunda
-  if [ -d "$ruta2" ]; then
-    copiar_archivos "$ruta2" "$destino"
-  else
-    echo "Ninguna de las rutas de datos existe."
-  fi
-fi
+echo "Archivos copiados exitosamente."
