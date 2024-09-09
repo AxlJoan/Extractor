f4e627cb995524c30502e3c444fa6cfa7f51220e
AxlJoan <60158935+AxlJoan@users.noreply.github.com>
2024-09-09 11:55:46 -0500

    Se añadió la instalación de dos2unix a la automatización

--- main.sh
+++ main.sh
@@ -3,6 +3,13 @@
 # Mantener el dispositivo despierto
 termux-wake-lock
 
+# Actualizar los repositorios y instalar dos2unix
+pkg update -y
+pkg install -y dos2unix
+
+# Convertir el archivo de formato DOS a formato UNIX
+dos2unix /data/data/com.termux/files/home/Extractor/sacarCombinado.sh
+
 # Esperar un minuto
 sleep 60
 
@@ -15,5 +22,5 @@ python /data/data/com.termux/files/home/Extractor/extractor2.py
 # Liberar el bloqueo de la pantalla
 termux-wake-unlock
 
-# Iniciar el servicio de cron
-# crond start (En revisión)
+# Iniciar el servicio de cron (Descomentar si es necesario y si el servicio está disponible en Termux)
+# crond start
